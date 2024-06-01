f5d414c7196de45a90f1442304e1a013d1506195
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-06-01 17:58:43 +0300

    Countdays

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [days, setDays] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date("June 01 2024");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <>
+      <div className="intervals">
+        <button onClick={() => setDays((d) => d + 1)} className="add">
+          Add +
+        </button>
+        Set Days: <span>{days}</span>
+        <button onClick={() => setDays((d) => d - 1)} className="sub">
+          Sub -
+        </button>
+      </div>
+
+      <div className="showdate">
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
+
+      <div className="increaseday">
+        <button onClick={() => setCount((c) => c + days)} className="add">
+          Add +
+        </button>
+        Count Days: <span>{count}</span>
+        <button onClick={() => setCount((c) => c - days)} className="sub">
+          Sub -
+        </button>
+      </div>
+    </>
+  );
+}
